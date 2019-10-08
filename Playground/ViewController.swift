9856011cc33e9248fd23671089ffa470522f6d50
Shivakumar Chandramouli <shivakumar.chandramouli@publicissapient.com>
2019-10-08 14:08:39 -0500

    Printed another helloworld

--- Playground/ViewController.swift
+++ Playground/ViewController.swift
@@ -18,5 +18,9 @@ class ViewController: UIViewController {
     func helloWorld() {
         print("HelloWorld")
     }
+    
+    func anotherHelloWorld() {
+        print("Another hello world")
+    }
 }
 
